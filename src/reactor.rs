8962159b1c86ee01b327b1d3de82737d30ea0a07
Wolfgang Bumiller <w.bumiller@proxmox.com>
2019-10-29 14:57:46 +0100

    working on stuff
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

---
+++ src/reactor.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::sync::{mpsc, Arc};
+use std::thread::JoinHandle;
+
+use crate::epoll::Epoll;
+
+pub struct AssertSync<T>(pub T);
+unsafe impl<T> Sync for AssertSync<T> {}
+
+pub struct Reactor {
+    epoll: Arc<Epoll>,
+    removals: AssertSync<mpsc::Sender<RawFd>>,
+    thread: JoinHandle<()>,
+}
+
+impl Reactor {
+    pub fn new() -> io::Error<Self> {
+        let epoll = Arc::new(Epoll::new()?);
+
+        let (send_remove, recv_remove) = mpsc::channel();
+
+        let handle = std::thread::spawn({
+            let epoll = Arc::clone(&epoll);
+            move || Self::thread_main(epoll, recv_remove)
+        });
+
+        Ok(Self {
+            epoll,
+            removals: AssertSync(send_remove),
+            thread: handle,
+        })
+    }
+
+    fn thread_main(epoll: Arc<Epoll>, removals: mpsc::Receiver<RawFd>) {
+        let _ = epoll;
+        let _ = removals;
+    }
+}
